9e3b4c8a75966cfe1d8ddede1695e7622479cafa
richardw16 <64897108+richardw16@users.noreply.github.com>
2026-01-05 23:28:50 +0100

    added example for exam template

--- examples/hello-world/data.mjs
+++ examples/hello-world/data.mjs
@@ -2,20 +2,19 @@ export default {
   debug: false, // keep temporary files for development, checks and styling with css
 
   docParts: [
-    { markdownFile: './testTitle.md', 
-      content: '../html/document-template-title.html' }, // relative to data file
+    { markdownFile: './testTitle.md', content: '../../html/document-template-title.html' }, // relative to data file
     {
       markdownFile: './testBody.md',
-      header: '../html/document-template-body.html.header.html',
-      content: '../html/document-template-body.html',
-      footer: '../html/document-template-body.html.footer.html',
+      header: '../../html/document-template-body.html.header.html',
+      content: '../../html/document-template-body.html',
+      footer: '../../html/document-template-body.html.footer.html',
     },
     // just trying for more that 2 parts - please note that puppeteer will start with page 1 again
     {
       markdownFile: './testBody.md',
-      header: '../html/document-template-body.html.header.html',
-      content: '../html/document-template-body.html',
-      footer: '../html/document-template-body.html.footer.html',
+      header: '../../html/document-template-body.html.header.html',
+      content: '../../html/document-template-body.html',
+      footer: '../../html/document-template-body.html.footer.html',
     },
   ],
   docTitle: 'test-md2pdf', // name of the final pdf
@@ -23,7 +22,7 @@ export default {
   authorHeader: 'Thomas Psik',
   // svg logos for header and footer - svg was the first thing that worked
   SVGContents: {
-    md2pdfLogoSVGContent: '../html/imgs/md2pdflogo.svg', // relative to data file
+    md2pdfLogoSVGContent: '../../html/imgs/md2pdflogo.svg', // relative to data file
   },
   // change margin to make room for the company logo
   topMargin: '20mm',
